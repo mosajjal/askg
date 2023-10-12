be33d80637997e5110e1dffb70e457e5a3faea25
Kiril Isakov <kirix.isakov@gmail.com>
2023-10-13 01:44:08 +0200

    remove left-out comments

--- common.go
+++ common.go
@@ -5,13 +5,12 @@ import (
 	"os"
 )
 
-// Reads input either from stdin or from the argument, if any.
+// Reads input from stdin.
 // Returns the input as a string.
 func Read_stdin() string {
 	stat, _ := os.Stdin.Stat()
 	var input_as_stdin string
 	if (stat.Mode() & os.ModeCharDevice) == 0 {
-		// Read input from stdin
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			input_as_stdin += scanner.Text()
